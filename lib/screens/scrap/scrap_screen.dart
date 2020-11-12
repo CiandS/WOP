f219143e44c008e8438a2571c1d0a2eecc587805
Kieran McCarthy <kieran.p.mccarthy@version1.com>
2020-11-12 12:55:30 +0000

    Added scrap screen

---
+++ lib/screens/scrap/scrap_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ScrapScreen extends StatefulWidget {
+  @override
+  _ScrapScreenState createState() => _ScrapScreenState();
+}
+
+class _ScrapScreenState extends State<ScrapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Work Order Process'),
+      ),
+      body: Container(),
+    );
+  }
+}
--- lib/screens/work_order_history/work_order_history.dart
+++ lib/screens/work_order_history/work_order_history.dart
@@ -6,43 +6,36 @@ class WO_HistoryPage extends StatefulWidget {
   @override
   _WO_HistoryState createState() => new _WO_HistoryState();
 }
+
 class _WO_HistoryState extends State<WO_HistoryPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.push(context, new MaterialPageRoute(
-                builder: (context) => WorkOrderPage()
-            ));
-          },),
-        appBar: AppBar(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              new MaterialPageRoute(builder: (context) => WorkOrderPage()));
+        },
+      ),
+      appBar: AppBar(
           title: new Center(
-            child: Text("Work Order History"),
-          )
-        ),
-       body: Container(
-         child: Center(
-           child: Column(
-             children: <Widget> [
-               DataTable(
-                   columns: [
-                      DataColumn(label: Text('Description')),
-                     DataColumn(label: Text('Date / Time'))
-               ], rows: [
-                 DataRow(cells: [
-                   DataCell(Text('JDE STATUS CHANGE TO 10. BY BOCONNO5')),
-                   DataCell(Text('16-OCT-2020 16:16'))
-                 ])
-               ])
-             ]
-           )
-           ),
-         ),
-      ));
-
+        child: Text("Work Order History"),
+      )),
+      body: Container(
+        child: Center(
+            child: Column(children: <Widget>[
+          DataTable(columns: [
+            DataColumn(label: Text('Description')),
+            DataColumn(label: Text('Date / Time'))
+          ], rows: [
+            DataRow(cells: [
+              DataCell(Text('JDE STATUS CHANGE TO 10. BY BOCONNO5')),
+              DataCell(Text('16-OCT-2020 16:16'))
+            ])
+          ])
+        ])),
+      ),
+    ));
   }
-
-
 }
