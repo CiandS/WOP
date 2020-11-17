60d3b868b3a66d5246804d3a15a6331e532ef7ec
deStaicC <cian.destaic@version1.com>
2020-11-17 16:10:31 +0000

    cleanline screen 0.0.1

--- lib/screens/work_order/widgets/cleanline.dart
+++ lib/screens/work_order/widgets/cleanline.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Cleanline extends StatelessWidget {
+class Cleanline extends StatefulWidget {
+  @override
+  _CleanlineState createState() => _CleanlineState();
+}
+
+class _CleanlineState extends State<Cleanline> {
+
+  int _cleaningMachine = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -9,14 +17,37 @@ class Cleanline extends StatelessWidget {
         children: <Widget>[
           Text("Cleanline\n",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          Text("Processing Amount", style: TextStyle(fontSize: 24)),
-          Text('Test', style: TextStyle(fontSize: 24, color: Colors.redAccent)),
+          Text('Attune Revise Inserts', style: TextStyle(fontSize: 24, color: Colors.redAccent)),
+      DropdownButton(
+          value: _cleaningMachine,
+          items: [
+            DropdownMenuItem(
+              child: Text("Please select your machine"),
+              value: 1,
+            ),
+            DropdownMenuItem(
+              child: Text("Machine 1"),
+              value: 2,
+            ),
+            DropdownMenuItem(child: Text("Machine 2"), value: 3),
+            DropdownMenuItem(
+              child: Text("Machine 3"),
+              value: 4,
+            ),
+          ],
+          onChanged: (value) {
+            setState(() {
+              _cleaningMachine = value;
+            });
+          }),
           TextFormField(
             decoration: InputDecoration(labelText: 'Input Amount'),
             keyboardType: TextInputType.number,
           )
+
         ],
       ),
     );
   }
+
 }
